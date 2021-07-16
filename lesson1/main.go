837fb432b65d0d71c603d6860462e3600041573b
Deny7676yar <73138994+Deny7676yar@users.noreply.github.com>
2021-07-16 22:28:05 +0300

    Update main.go
    исправление ошибки

--- lesson1/main.go
+++ lesson1/main.go
@@ -5,23 +5,36 @@ import (
 	"fmt"
 	"log"
 	"time"
-	""
-
-
 )
+
+// тип ошибки раящий время возиковеия
+type ErrorDate struct {
+	time.Time
+}
+//создаем экземпляр ошибки
+func NewErrTime() *ErrorDate {
+	return &ErrorDate{
+		Time: time.Now(),
+	}
+}
+//реализация фукции Error из итерфейса
+func (e *ErrorDate) Error()string  {
+	return fmt.Sprintf("an error occurred %s", e.Format("2006-01-02 15:04:05"))
+}
+
 
 /*
 Функция простая в ней содержится ошибка,
 в цикле <= вместо просто <
- */
+*/
 func main() {
 	var (
 		ErrComparison = errors.New("error in work with comparison")
 	)
 	defer func() {
 		if r := recover();r !=nil {
-			dt := time.Now()
-			log.Println(fmt.Errorf("%w: %s", ErrComparison, r),dt)
+
+			log.Println(fmt.Errorf("%w: %s", ErrComparison, NewErrTime()))
 		}
 		planB()
 	}()
@@ -31,6 +44,7 @@ func main() {
 	x[1] = 93
 	x[2] = 77
 
+
 	var total float64 = 0
 	for i := 0; i <= 5; i++ {
 		total += x[i]
@@ -38,6 +52,20 @@ func main() {
 	fmt.Println(total / 5)
 
 
+
+}
+//фукция без неверого знака
+func planB() {
+	var x [5]float64
+	x[0] = 98
+	x[1] = 93
+	x[2] = 77
+
+	var total float64 = 0
+	for i := 0; i < 5; i++ {
+		total += x[i]
+	}
+	fmt.Println(total / 5)
 
 }
 
